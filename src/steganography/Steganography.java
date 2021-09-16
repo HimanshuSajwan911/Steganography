0304430a5fdd836ea4e996844f182e39b2356bc3
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-09-16 18:35:36 +0530

    added extractByte function.

--- src/steganography/Steganography.java
+++ src/steganography/Steganography.java
@@ -1,5 +1,6 @@
 package steganography;
 
+import SteganographyExceptions.InsufficientBitsException;
 import SteganographyExceptions.InsufficientMemoryException;
 
 /**
@@ -24,9 +25,60 @@ public class Steganography {
      */
     public static final int CANCELED = -1;
 
-    private void addBits(byte[] target, byte[] source) throws InsufficientMemoryException {
+    /**
+     * Returns byte array of <B>amount</B> number of bytes extracted from <B>position</B> of <B>source</B> byte array.
+     * 
+     * @param source byte array which contains bytes to be extracted.
+     * @param position from where bytes are to be extracted.
+     * @param amount number of bytes to be extracted.
+     * @return byte array of extracted bytes.
+     * @throws InsufficientBitsException
+     */
+    public static byte[] extractByte(byte[] source, int position, int amount) throws InsufficientBitsException {
 
-        if (target.length < source.length * 8) {
+        if(amount < 1){
+            throw new InsufficientBitsException("number of byte cannot be less than 1.");
+        }
+        
+        // if source array does not contain enough bits.
+        if ((amount * 8 + position) > source.length) {
+            throw new InsufficientBitsException("source does not contain specified bytes.");
+        }
+
+        byte[] result = new byte[amount];
+
+        for (int i = position, pos = 0; i < amount; i++, pos++) {
+
+            byte retrieved_byte = 0;
+
+            for (int j = 0; j < 7; j++, position++) {
+
+                byte source_byte = source[position];
+                retrieved_byte = (byte) ((retrieved_byte << 1) | (source_byte & 1));
+
+            }
+            
+            result[pos] = retrieved_byte;
+            
+        }
+ 
+        return result;
+    }
+
+    
+    /**
+     * Insert all the bits of source byte array (starting from source_position) in
+     * <B>LSB</B> position of target byte array (starting from target_position).
+     * 
+     * @param target byte array in which bytes are to be inserted.
+     * @param target_position starting position for target array from where bits are to be inserted.
+     * @param source byte array from where bytes are to be inserted in target byte array.
+     *
+     * @throws InsufficientMemoryException
+    **/
+    public static void addBits(byte[] target, int target_position, byte[] source) throws InsufficientMemoryException {
+
+        if (target.length < (source.length * 8 + target_position)) {
             throw new InsufficientMemoryException();
         }
 
@@ -34,9 +86,9 @@ public class Steganography {
         for (int i = 0; i < source.length; i++) {
             byte data = source[i];
 
-            // loop to extract all bits of data byte.
-            for (int j = 7; j >= 0; j--, Current_Position++) {
-                target[Current_Position] = (byte) ((target[Current_Position] & 0xFE) | ((data >> j) & 1));
+            // loop to extract all bits of byte "data".
+            for (int j = 7; j >= 0; j--, target_position++) {
+                target[target_position] = (byte) ((target[target_position] & 0xFE) | ((data >> j) & 1));
             }
 
         }
