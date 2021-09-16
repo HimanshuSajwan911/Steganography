068ef00ba1a2377519e57ec28c3cb7b812f445a3
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-09-16 18:48:07 +0530

    InsufficientMemoryException Exception class.

---
+++ src/steganography/exceptions/InsufficientMemoryException.java
@@ -0,0 +1,19 @@
+
+package steganography.exceptions;
+
+/**
+ * @author Himanshu Sajwan.
+ */
+
+// InsufficientMemoryException Exception class
+public class InsufficientMemoryException extends Exception{
+
+    public InsufficientMemoryException() {
+        super("Insufficient Memory");
+    }
+    
+    public InsufficientMemoryException(String msg) {
+        super(msg);
+    }
+    
+}
