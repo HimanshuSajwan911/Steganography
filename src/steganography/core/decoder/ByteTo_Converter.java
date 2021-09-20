f56b7a8829ee2a6b8a32c038d27efc0a52c9095f
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-09-20 20:11:48 +0530

    convert byte array to various datatype.

--- src/steganography/core/decoder/ByteTo_Converter.java
+++ src/steganography/core/decoder/ByteTo_Converter.java
@@ -6,24 +6,36 @@ package steganography.core.decoder;
 
 public class ByteTo_Converter {
 
-    public static int byteToInt(byte[] source){
-        
-        if(source.length != 4){
+    /**
+     * Converts byte array to int.
+     *
+     * @param source
+     * <P>
+     * byte array which needs to be converted to int.</P>
+     * <P>
+     * (must contain only 4 bytes.)</P>
+     * @return int value of byte array.
+     *
+     * @throws IllegalArgumentException
+     */
+    public static int byteToInt(byte[] source) throws IllegalArgumentException {
+
+        if (source.length != 4) {
             throw new IllegalArgumentException("int have only 4 bytes.");
         }
-        
+
         int result = 0;
-        
-        for(int i = 0; i < 4; i++){
-            
+
+        for (int i = 0; i < 4; i++) {
+
             byte data = source[i];
-            
-            for(int j = 7; j >= 0; j--){
+
+            for (int j = 7; j >= 0; j--) {
                 result = (result << 1) | (data & 1);
             }
         }
-        
+
         return result;
-    } 
-    
+    }
+
 }
