54e6d265b57566acc5891580446c242696063f57
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-10-09 11:21:51 +0530

    added decode function.

--- src/steganography/DocumentSteganography.java
+++ src/steganography/DocumentSteganography.java
@@ -7,7 +7,12 @@ import static steganography.core.Steganography.LENGTH_SIZE;
 import static steganography.core.Steganography.addKey;
 import static steganography.core.Steganography.addLength;
 import static steganography.core.Steganography.addMessage;
+import static steganography.core.Steganography.getKey;
+import static steganography.core.Steganography.getMessage;
+import static steganography.core.Steganography.getMessageLength;
+import steganography.core.exceptions.InsufficientBitsException;
 import steganography.core.exceptions.InsufficientMemoryException;
+import steganography.core.exceptions.InvalidKeyException;
 import static steganography.core.filehandling.Reader.readBytes;
 import static steganography.core.filehandling.Writer.writeBytes;
 
@@ -24,8 +29,8 @@ public class DocumentSteganography {
     */
 
     /**
-     * Function to encode Document from <B>source_full_path</B> location 
-     * with <B>Message</b> message and save this encoded document to <B>destination_full_path</B> location.
+     * Encode Document from <B>source_full_path</B> location 
+     * with <B>Message</b> and save this encoded document to <B>destination_full_path</B> location.
      * 
      * @param source_full_path location of source Document file.
      * @param destination_full_path location to save encoded Document file.
@@ -65,5 +70,42 @@ public class DocumentSteganography {
 
     }
    
+    /**
+     * Decode Document from <B>source_full_path</B> location using <B>key</B>
+     * and return encoded message from file.
+     * 
+     * @param source_full_path location of source Document file.
+     * @param key to access encoded Document file with a 32 bit size integer.
+     * 
+     * @throws IOException
+     * @throws InsufficientBitsException 
+     * @throws InvalidKeyException 
+     */
+    public String decode(String source_full_path, int key) throws IOException, InsufficientBitsException, InvalidKeyException{
+        
+        byte[] doc_bytes = readBytes(source_full_path);
+        
+        int position = 0;
+        
+        int extracted_key = getKey(doc_bytes, position, KEY_SIZE);
+        
+        if(extracted_key != key){
+            throw new InvalidKeyException();
+        }
+        
+        // key took (KEY_SIZE * 8) positions ie (4 bytes * 8) = 32 bits position.
+        position += (KEY_SIZE * 8);
+        
+        int length = getMessageLength(doc_bytes, position, LENGTH_SIZE);
+        
+        // length took (LENGTH_SIZE * 8) positions ie (4 bytes * 8) = 32 bits position.
+        position += (LENGTH_SIZE * 8);
+        
+        String message = getMessage(doc_bytes, position, length);
+        
+        return message;
+        
+    }
+    
     
 }
