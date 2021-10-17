959b4db2cb89303d9b53dd3e0c793c933342ffb3
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-10-17 16:18:45 +0530

    added parameters target_start_position and target_end_position.

--- src/steganography/core/encoder/SteganographyEncoder.java
+++ src/steganography/core/encoder/SteganographyEncoder.java
@@ -10,10 +10,12 @@ public class SteganographyEncoder {
     /**
      * Insert all the bits of source byte array (starting from <B>source_start_position</B> till <B>source_end_position</B>)
      * in
-     * <B>LSB</B> position of target byte array (starting from <B>target_position</B>).
+     * <B>LSB</B> position of target byte array (starting from <B>target_start_position</B> till <B>target_end_position</B>).
      *
      * @param target byte array in which bytes are to be inserted.
-     * @param target_position starting position for target array, where bits
+     * @param target_start_position starting position for target array, where bits
+     * are to be inserted.
+     * @param target_end_position ending position for target array, till where bits
      * are to be inserted.
      * @param source byte array from where bytes are to be inserted in target
      * byte array.
@@ -23,11 +25,12 @@ public class SteganographyEncoder {
      * @throws InsufficientMemoryException
     *
      */
-    public static void addBits(byte[] target, int target_position, byte[] source, int source_start_position, int source_end_position) throws InsufficientMemoryException {
+    public static void addBits(byte[] target, int target_start_position, int target_end_position, byte[] source, int source_start_position, int source_end_position) throws InsufficientMemoryException {
 
         int source_length = source_end_position - source_start_position;
+        int target_length = target_end_position - target_start_position;
         
-        if (target.length < (source_length * 8 + target_position)) {
+        if (target_length < (source_length * 8 + target_start_position)) {
             throw new InsufficientMemoryException();
         }
 
@@ -36,8 +39,8 @@ public class SteganographyEncoder {
             byte data = source[i];
 
             // loop to extract all bits of byte "data".
-            for (int j = 7; j >= 0; j--, target_position++) {
-                target[target_position] = (byte) ((target[target_position] & 0xFE) | ((data >>> j) & 1));
+            for (int j = 7; j >= 0; j--, target_start_position++) {
+                target[target_start_position] = (byte) ((target[target_start_position] & 0xFE) | ((data >>> j) & 1));
             }
 
         }
