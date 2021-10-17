68a64f0949c586442baea15b3f37fe700ceed009
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-10-17 18:51:28 +0530

    added byteToLong function.

--- src/steganography/core/decoder/ByteTo_Converter.java
+++ src/steganography/core/decoder/ByteTo_Converter.java
@@ -9,7 +9,7 @@ public class ByteTo_Converter {
     /**
      * Converts byte array to int.
      *
-     * @param source
+     * @param source array containing bytes that needs to be converted to int.
      * <P>
      * byte array which needs to be converted to int.</P>
      * <P>
@@ -27,6 +27,38 @@ public class ByteTo_Converter {
         int result = 0;
 
         for (int i = 0; i < 4; i++) {
+
+            byte data = source[i];
+
+            for (int j = 7; j >= 0; j--) {
+                result = (result << 1) | ((data >>> j) & 1);
+            }
+        }
+
+        return result;
+    }
+    
+    /**
+     * Converts byte array to long.
+     *
+     * @param source array containing bytes that needs to be converted to long.
+     * <P>
+     * byte array which needs to be converted to int.</P>
+     * <P>
+     * (must contain only 8 bytes.)</P>
+     * @return int value of byte array.
+     *
+     * @throws IllegalArgumentException
+     */
+    public static long byteToLong(byte[] source) throws IllegalArgumentException {
+
+        if (source.length != 8) {
+            throw new IllegalArgumentException("int can have only 4 bytes.");
+        }
+
+        long result = 0;
+
+        for (int i = 0; i < 8; i++) {
 
             byte data = source[i];
 
