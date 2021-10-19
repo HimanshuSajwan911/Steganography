585fc0f5afd738010ad6db1da19e028fdf1d4c93
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-10-19 18:22:59 +0530

    added skip() function.

--- src/steganography/core/filehandling/Writer.java
+++ src/steganography/core/filehandling/Writer.java
@@ -3,6 +3,8 @@ package steganography.core.filehandling;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * @author Himanshu Sajwan.
@@ -26,6 +28,27 @@ public class Writer {
             stream.write(source);
         }
 
+    }
+    
+    /**
+     * Skips ie reads <B>amount</B> number of bytes from <B>input</B> InputStream and writes those bytes to <B>output</B> OutputStream.
+     * 
+     * @param input InputStream object from where bytes are to be read.
+     * @param output OutputStream object to where bytes are to be written.
+     * @param amount number of bytes that to be read and written.
+     * @throws IOException 
+     */
+    
+    public static void skip(InputStream input, OutputStream output, int amount) throws IOException{
+        byte[] b = new byte[amount];
+        
+        if (input != null) {
+            input.read(b);
+        }
+        
+        if (output != null) {
+            output.write(b);
+        }
     }
 
 }
