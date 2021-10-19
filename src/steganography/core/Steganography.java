af62c06cbc395775a647ae404691c1b9d6613b6a
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-10-19 23:14:16 +0530

    modified functions.

--- src/steganography/core/Steganography.java
+++ src/steganography/core/Steganography.java
@@ -2,6 +2,7 @@
 package steganography.core;
 
 import static steganography.core.decoder.ByteTo_Converter.byteToInt;
+import static steganography.core.decoder.ByteTo_Converter.byteToLong;
 import static steganography.core.decoder.SteganographyDecoder.extractByte;
 import static steganography.core.encoder.SteganographyEncoder.addBits;
 import static steganography.core.encoder._ToByteConverter.intToByte;
@@ -52,14 +53,14 @@ public class Steganography {
         addBits(source, position, position + KEY_SIZE_BIT , keyBytes, 0, keyBytes.length);
     }
 
-    public static void addLength(byte[] source, int position, long length) throws InsufficientMemoryException {
+    public static void addMessageLength(byte[] source, int position, long length) throws InsufficientMemoryException {
         byte[] lengthBytes = longToByte(length);
 
         addBits(source, position, position + LENGTH_SIZE_BIT, lengthBytes, 0, lengthBytes.length);
     }
 
     public static void addMessage(byte[] source, int position, byte[] message) throws InsufficientMemoryException {
-        addBits(source, position, message, 0, message.length);
+        addBits(source, position, source.length, message, 0, message.length);
     }
    
     /*
@@ -84,14 +85,14 @@ public class Steganography {
         return res;
     }
     
-    public static int getMessageLength(byte[] source, int position, int length_size) throws InsufficientBitsException{
+    public static long getMessageLength(byte[] source, int position, int length_size) throws InsufficientBitsException{
         byte[] length_bytes = extractByte(source, position, length_size);
         
-        return byteToInt(length_bytes);
+        return byteToLong(length_bytes);
     }
     
-    public static String getMessage(byte[] source, int position, int message_length) throws InsufficientBitsException{
-        return new String(extractByte(source, position, message_length));
+    public static byte[] getMessage(byte[] source, int position, int message_length) throws InsufficientBitsException{
+        return extractByte(source, position, message_length);
     }
     
     /*
