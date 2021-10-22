2d58ae23c7d23d2a2e4106be60020c3746459720
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-10-22 16:10:44 +0530

    added checking if source_file and data_file exists.

--- src/steganography/AudioSteganography.java
+++ src/steganography/AudioSteganography.java
@@ -30,6 +30,7 @@ import static steganography.core.filehandling.Writer.skip;
 public class AudioSteganography extends Steganography{
 
     public AudioSteganography(){
+        // setting default value for SOURCE_BUFFER_SIZE.
         SOURCE_BUFFER_SIZE = MB; // 1 MB
         DATA_BUFFER_SIZE = (SOURCE_BUFFER_SIZE / 8); // 128 KB
     }
@@ -43,6 +44,14 @@ public class AudioSteganography extends Steganography{
         
         File src_file = new File(sourceFile_full_path);
         File data_file = new File(dataFile_full_path);
+        
+        if(!src_file.exists()){
+            throw new FileNotFoundException("(The system cannot find the source file specified)");
+        }
+        
+        if(!data_file.exists()){
+            throw new FileNotFoundException("(The system cannot find the data file specified)");
+        }
         
         if(src_file.length() < data_file.length() * 8){
             throw new InsufficientMemoryException("not enough space in source file!!");
@@ -152,6 +161,10 @@ public class AudioSteganography extends Steganography{
     
      
     public void decode(String sourceFile_full_path, String destinationFile_full_path, int key) throws UnsupportedAudioFileException, IOException, FileNotFoundException, InsufficientBitsException, InvalidKeyException{
+        
+        if(!new File(sourceFile_full_path).exists()){
+            throw new FileNotFoundException("(The system cannot find the source file specified)");
+        }
         
         String extension = Filters.getFileExtension(new File(sourceFile_full_path));
         
