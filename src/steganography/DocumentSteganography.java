bc06b54e88360da43d7392155829fc118995a90b
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-10-23 18:21:10 +0530

    changed encode and decode function. added encodeTxt and decodeTxt function for text file.

--- src/steganography/DocumentSteganography.java
+++ src/steganography/DocumentSteganography.java
@@ -1,26 +1,37 @@
 package steganography;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import static steganography.core.Steganography.KEY_SIZE;
-import static steganography.core.Steganography.LENGTH_SIZE;
+import steganography.core.Steganography;
 import static steganography.core.Steganography.addKey;
-import static steganography.core.Steganography.addLength;
-import static steganography.core.Steganography.addMessage;
 import static steganography.core.Steganography.getKey;
 import static steganography.core.Steganography.getMessage;
 import static steganography.core.Steganography.getMessageLength;
 import steganography.core.exceptions.InsufficientBitsException;
 import steganography.core.exceptions.InsufficientMemoryException;
 import steganography.core.exceptions.InvalidKeyException;
-import static steganography.core.filehandling.Reader.readBytes;
-import static steganography.core.filehandling.Writer.writeBytes;
+import static steganography.core.Steganography.KEY_SIZE_BYTE;
+import static steganography.core.Steganography.addMessageLength;
+import static steganography.core.encoder.SteganographyEncoder.addBits;
+import steganography.core.exceptions.UnsupportedDocumentFileException;
+import steganography.core.filehandling.Filters;
+import static steganography.core.filehandling.Writer.skip;
 
 /**
  * @author Himanshu Sajwan.
  */
 
-public class DocumentSteganography {
+public class DocumentSteganography extends Steganography{
+
+    public DocumentSteganography(){
+        // setting default value for SOURCE_BUFFER_SIZE.
+        SOURCE_BUFFER_SIZE = MB; // 1 MB
+        DATA_BUFFER_SIZE = (SOURCE_BUFFER_SIZE / 8); // 128 KB
+    }
     
     
     
@@ -29,47 +40,132 @@ public class DocumentSteganography {
     */
 
     /**
-     * Encode Document from <B>source_full_path</B> location 
-     * with <B>Message</b> and save this encoded document to <B>destination_full_path</B> location.
+     * Encode Document from <B>sourceFile_full_path</B> location 
+     * with file from <B>dataFile_full_path</B> and save this encoded document to <B>destinationFile_full_path</B> location.
      * 
-     * @param source_full_path location of source Document file.
-     * @param destination_full_path location to save encoded Document file.
+     * @param sourceFile_full_path location of source Document file.
+     * @param dataFile_full_path location of data file that is to be encoded.
+     * @param destinationFile_full_path location to save encoded Document file.
      * @param key to secure encoded file with a 32 bit size integer.
-     * @param message to be encoded into the Document file.
      * 
      * @throws IOException
      * @throws InsufficientMemoryException
      * @throws UnsupportedAudioFileException
      */
-
-    public void encode(String source_full_path, String destination_full_path, int key, String message) throws IOException, InsufficientMemoryException, UnsupportedAudioFileException {
-
-        byte[] audio_bytes = readBytes(source_full_path);
-
-        byte[] Message = message.getBytes();
-
-        int position = 0;
+    public void encode(String sourceFile_full_path, String dataFile_full_path, String destinationFile_full_path, int key) throws IOException, InsufficientMemoryException, UnsupportedAudioFileException {
         
-        // adding secert key to audio byte.
-        addKey(audio_bytes, position, key);
+        File src_file = new File(sourceFile_full_path);
+        File data_file = new File(dataFile_full_path);
+        
+        if(!src_file.exists()){
+            throw new FileNotFoundException("(The system cannot find the source file specified)");
+        }
+        
+        if(!data_file.exists()){
+            throw new FileNotFoundException("(The system cannot find the data file specified)");
+        }
+        
+        if(src_file.length() < data_file.length() * 8){
+            throw new InsufficientMemoryException("not enough space in source file!!");
+        }
+        
+        String extension = Filters.getFileExtension(src_file);
+        
+        switch(extension){
+           
+            case "txt": encodeTxt(sourceFile_full_path, dataFile_full_path, destinationFile_full_path, key);
+                        break;
+                                
+                       
+                        
+            default:    throw new UnsupportedAudioFileException("'" + extension +"' file format is not yet supported.");
+          
+        }
+        
+    }
+    
+    private void encodeTxt(String sourceFile_full_path,String dataFile_full_path, String destinationFile_full_path, int key) throws IOException, InsufficientMemoryException, UnsupportedAudioFileException {
 
-        // key took (KEY_SIZE * 8) positions ie (4 bytes * 8) = 32 bits position.
-        position += KEY_SIZE * 8;
-        
-        // adding message length to audio byte.
-        addLength(audio_bytes, position, Message.length);
-        
-        // length took (LENGTH_SIZE * 8) positions ie (4 bytes * 8) = 32 bits position.
-        position += LENGTH_SIZE * 8;
+        try (
+            FileInputStream  source_input_Stream = new FileInputStream(sourceFile_full_path);
+            FileInputStream  data_input_Stream   = new FileInputStream(dataFile_full_path);
+            FileOutputStream output_Stream       = new FileOutputStream(destinationFile_full_path);
+        ) {
 
-        // adding message.
-        addMessage(audio_bytes, position, Message);
-        
-        // saving audio_byte array as a file.
-        writeBytes(audio_bytes, destination_full_path);
+            // length of data file.
+            long length = new File(dataFile_full_path).length();
+            
+            // to store source byte stream.
+            byte[] source;
+            
+            // to store data byte stream.
+            byte[] data; 
+            
+            // ----------------------------adding key start--------------------------//
+            source = new byte[KEY_SIZE_BIT];
+            
+            // reading 32 bytes.
+            source_input_Stream.read(source);
+            
+            // inserting 32 bit key in LSB of 32 bytes.
+            addKey(source, 0, key);
+            
+            // writing these encoded 32 bytes to output file.
+            output_Stream.write(source);
+            
+            // ----------------------------adding key ends--------------------------//
+            
+            
+            // ----------------------------adding length start--------------------------//
+            source = new byte[LENGTH_SIZE_BIT];
+            
+            // reading 64 bytes.
+            source_input_Stream.read(source);
+            
+            // inserting 64 bit length in LSB of 64 bytes.
+            addMessageLength(source, 0, length);
+            
+            // writing these encoded 64 bytes to output file.
+            output_Stream.write(source);
+            
+            
+            // ----------------------------adding length ends--------------------------//
+            
+            
+            // ----------------------------adding data starts--------------------------//
+            data = new byte[DATA_BUFFER_SIZE];
+            source = new byte[SOURCE_BUFFER_SIZE];
+            
+            int noOfSourceBytes, noOfDataBytes;
+            
+            // while source has bytes.
+            while ((noOfSourceBytes = source_input_Stream.read(source)) > 0) {
+               
+                // if data bytes exists.
+                if((noOfDataBytes = data_input_Stream.read(data)) > 0){
+                    addBits(source, 0, source.length, data, 0, noOfDataBytes);
+                }
+                
+                output_Stream.write(source, 0, noOfSourceBytes);
+                
+            }
+ 
+            // ----------------------------adding data ends--------------------------//
+
+        }
 
     }
+    
+    /*
+        ----------------------------------------Encoding part ends here----------------------------------------
+    */
    
+    
+    /*
+        ----------------------------------------Decoding part starts here----------------------------------------
+    */
+    
+    
     /**
      * Decode Document from <B>source_full_path</B> location using <B>key</B>
      * and return encoded message from file.
@@ -81,31 +177,95 @@ public class DocumentSteganography {
      * @throws InsufficientBitsException 
      * @throws InvalidKeyException 
      */
-    public String decode(String source_full_path, int key) throws IOException, InsufficientBitsException, InvalidKeyException{
+    public void decode(String sourceFile_full_path, String destinationFile_full_path, int key) throws IOException, InsufficientBitsException, InvalidKeyException, UnsupportedDocumentFileException{
         
-        byte[] doc_bytes = readBytes(source_full_path);
-        
-        int position = 0;
-        
-        int extracted_key = getKey(doc_bytes, position, KEY_SIZE);
-        
-        if(extracted_key != key){
-            throw new InvalidKeyException();
+        if(!new File(sourceFile_full_path).exists()){
+            throw new FileNotFoundException("(The system cannot find the source file specified)");
         }
         
-        // key took (KEY_SIZE * 8) positions ie (4 bytes * 8) = 32 bits position.
-        position += (KEY_SIZE * 8);
+        String extension = Filters.getFileExtension(new File(sourceFile_full_path));
         
-        int length = getMessageLength(doc_bytes, position, LENGTH_SIZE);
-        
-        // length took (LENGTH_SIZE * 8) positions ie (4 bytes * 8) = 32 bits position.
-        position += (LENGTH_SIZE * 8);
-        
-        String message = getMessage(doc_bytes, position, length);
-        
-        return message;
+        switch(extension){
+           
+            case "txt": decodeTxt(sourceFile_full_path,destinationFile_full_path, key);
+                        break;
+                                
+                       
+                        
+            default:    throw new UnsupportedDocumentFileException("'" + extension +"' file format is not yet supported.");
+          
+        }
         
     }
     
+    private void decodeTxt(String sourceFile_full_path, String destinationFile_full_path, int key) throws FileNotFoundException, IOException, InsufficientBitsException, InvalidKeyException{
+        
+        try (
+            FileInputStream  source_input_Stream = new FileInputStream(sourceFile_full_path);
+            FileOutputStream output_Stream       = new FileOutputStream(destinationFile_full_path);
+        ) {
+            
+            byte[] source; // to store source byte stream.
+            
+            // ----------------------------decoding key start--------------------------//
+            source = new byte[KEY_SIZE_BIT];
+            
+            // reading 32 bytes.
+            source_input_Stream.read(source);
+            
+            // extracting 4 byte (32 bit) key from LSB of 32 bytes.
+            int extracted_key = getKey(source, 0, KEY_SIZE_BYTE);
+            
+            if(extracted_key != key){
+                throw new InvalidKeyException();
+            }
+            
+            
+            // ----------------------------decoding key ends--------------------------//
+            
+            
+            // ----------------------------decoding length start--------------------------//
+            source = new byte[LENGTH_SIZE_BIT];
+            
+            // reading 64 bytes.
+            source_input_Stream.read(source);
+            
+            // extracting 8 byte (64 bit) length from LSB of 64 bytes.
+            long length = getMessageLength(source, 0, LENGTH_SIZE_BYTE);
+            
+            // writing these encoded 64 bytes to output file.
+            
+            
+            // ----------------------------decoding length ends--------------------------//
+            
+            
+            // ----------------------------decoding data starts--------------------------//
+            source = new byte[SOURCE_BUFFER_SIZE];
+            
+            int extract_length;
+            
+            while(length > 0){
+                
+                if(length <= DATA_BUFFER_SIZE){
+                    extract_length = (int)length;
+                }
+                else{
+                    extract_length = DATA_BUFFER_SIZE;
+                }
+                source_input_Stream.read(source);
+                
+                byte[] extracted_data = getMessage(source, 0,  extract_length);
+                
+                output_Stream.write(extracted_data);
+                length -= extract_length;
+            }
+            
+            // ----------------------------decoding data ends--------------------------//
+        } 
+        
+    }
     
+    /*
+        ----------------------------------------Decoding part ends here----------------------------------------
+    */
 }
