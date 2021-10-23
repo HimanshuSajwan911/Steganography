02d1815548efc79d3ec44be97d850fb14f6e2a5a
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-10-23 17:55:49 +0530

    UnsupportedDocumentFileException Exception class.

---
+++ src/steganography/core/exceptions/UnsupportedDocumentFileException.java
@@ -0,0 +1,20 @@
+package steganography.core.exceptions;
+
+/**
+ * @author Himanshu Sajwan.
+ */
+
+/**
+ * UnsupportedDocumentFileException Exception class.
+ */
+public class UnsupportedDocumentFileException extends Exception{
+
+    public UnsupportedDocumentFileException() {
+        
+    }
+
+    public UnsupportedDocumentFileException(String msg) {
+        super(msg);
+    }
+    
+}
