441ee681bc15610d66b22ced14b602f74c26c599
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-11-04 17:37:07 +0530

    added encodeMP4() function.

--- src/steganography/VideoSteganography.java
+++ src/steganography/VideoSteganography.java
@@ -1,14 +1,24 @@
 package steganography;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import javafx.util.Pair;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import steganography.core.Steganography;
+import static steganography.core.Steganography.KEY_SIZE_BIT;
+import static steganography.core.Steganography.LENGTH_SIZE_BIT;
+import static steganography.core.Steganography.addKey;
+import static steganography.core.Steganography.addMessageLength;
+import static steganography.core.encoder.SteganographyEncoder.addBits;
 import steganography.core.exceptions.InsufficientBitsException;
 import steganography.core.exceptions.InsufficientMemoryException;
 import steganography.core.exceptions.InvalidKeyException;
 import steganography.core.filehandling.Filters;
+import static steganography.core.filehandling.Writer.skip;
+import steganography.core.util.MP4;
 
 /**
  * @author Himanshu Sajwan.
@@ -61,7 +71,90 @@ public class VideoSteganography extends Steganography{
         
     }
     
-    
+    private void encodeMP4(String sourceFile_full_path,String dataFile_full_path, String destinationFile_full_path, int key) throws IOException, InsufficientMemoryException, UnsupportedAudioFileException {
+
+        try (
+            FileInputStream  source_input_Stream = new FileInputStream(sourceFile_full_path);
+            FileInputStream  data_input_Stream   = new FileInputStream(dataFile_full_path);
+            FileOutputStream output_Stream       = new FileOutputStream(destinationFile_full_path);
+        ) {
+            
+            // length of data file.
+            long data_file_length = new File(dataFile_full_path).length();
+            
+            // to store source byte stream.
+            byte[] source = new byte[SOURCE_BUFFER_SIZE];
+            
+            // to store data byte stream.
+            byte[] data; 
+            
+            int noOfSourceBytes, noOfDataBytes;
+           
+            boolean found = false;
+            
+            MP4 mp4 = new MP4(sourceFile_full_path);
+            
+            int position = mp4.getMdat_position();
+            long source_length = mp4.getMdat_SIZE();
+             
+             if(source_length < (data_file_length * 8) + KEY_SIZE_BIT + LENGTH_SIZE_BIT){
+                throw new InsufficientMemoryException("not enough space in source file!!");
+             }
+             
+             // skips modifying source header.
+             skip(source_input_Stream, output_Stream, position);
+             
+            // ----------------------------adding key start--------------------------//
+            source = new byte[KEY_SIZE_BIT];
+            
+            // reading 32 bytes.
+            source_input_Stream.read(source);
+            
+            // inserting 32 bit key in LSB of 32 bytes.
+            addKey(source, 0, key);
+            
+            // writing these encoded 32 bytes to output file.
+            output_Stream.write(source);
+            
+            // ----------------------------adding key ends--------------------------//
+            
+            
+            // ----------------------------adding length start--------------------------//
+            source = new byte[LENGTH_SIZE_BIT];
+            
+            // reading 64 bytes.
+            source_input_Stream.read(source);
+            
+            // inserting 64 bit length in LSB of 64 bytes.
+            addMessageLength(source, 0, data_file_length);
+            
+            // writing these encoded 64 bytes to output file.
+            output_Stream.write(source);
+            
+            
+            // ----------------------------adding length ends--------------------------//
+            
+            
+            // ----------------------------adding data starts--------------------------//
+            data = new byte[DATA_BUFFER_SIZE];
+            source = new byte[SOURCE_BUFFER_SIZE];
+            
+            // while source has bytes.
+            while ((noOfSourceBytes = source_input_Stream.read(source)) > 0) {
+               
+                // if data bytes exists.
+                if((noOfDataBytes = data_input_Stream.read(data)) > 0){
+                    addBits(source, 0, source.length, data, 0, noOfDataBytes);
+                }
+                
+                output_Stream.write(source, 0, noOfSourceBytes);
+                
+            }
+ 
+            // ----------------------------adding data ends--------------------------//
+        } 
+
+    }
     
 
 
@@ -95,10 +188,6 @@ public class VideoSteganography extends Steganography{
         }
         
     }
-    
-        
-    }
-    
     
     /*
         ________________________________________Decoding part ends here_________________________________________
