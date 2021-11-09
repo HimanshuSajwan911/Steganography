a2f587be33263e8f6dc690e7452fdc2f41660be8
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-11-09 23:05:46 +0530

    UnsupportedVideoFileException Exception class.

---
+++ src/steganography/core/exceptions/UnsupportedVideoFileException.java
@@ -0,0 +1,20 @@
+package steganography.core.exceptions;
+
+/**
+ * @author Himanshu Sajwan.
+ */
+
+/**
+ * UnsupportedVideoFileException Exception class.
+ */
+public class UnsupportedVideoFileException extends UnsupportedFileException{
+
+    public UnsupportedVideoFileException() {
+        
+    }
+
+    public UnsupportedVideoFileException(String msg) {
+        super(msg);
+    }
+    
+}
