fc5fff5bad7a00d6084c3e59943983c1524c411f
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-11-09 23:03:41 +0530

    UnsupportedAudioFileException Exception class.

---
+++ src/steganography/core/exceptions/UnsupportedAudioFileException.java
@@ -0,0 +1,20 @@
+package steganography.core.exceptions;
+
+/**
+ * @author Himanshu Sajwan.
+ */
+
+/**
+ * UnsupportedAudioFileException Exception class.
+ */
+public class UnsupportedAudioFileException extends UnsupportedFileException{
+
+    public UnsupportedAudioFileException() {
+        
+    }
+
+    public UnsupportedAudioFileException(String msg) {
+        super(msg);
+    }
+    
+}
