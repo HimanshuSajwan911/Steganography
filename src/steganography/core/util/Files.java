8abce48e1ca97eddf349a28a606cb6aaf9ffeaa5
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-11-17 18:29:03 +0530

    added getFileExtension() function.

--- src/steganography/core/util/Files.java
+++ src/steganography/core/util/Files.java
@@ -1,6 +1,7 @@
 
 package steganography.core.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -19,7 +20,6 @@ public class Files {
      * @param amount number of bytes that to be read and written.
      * @throws IOException 
      */
-    
     public static void skip(InputStream input, OutputStream output, int amount) throws IOException{
         byte[] b = new byte[amount];
         
@@ -30,6 +30,21 @@ public class Files {
         if (output != null) {
             output.write(b);
         }
+    }
+    
+    /**
+     * Returns extension of file.
+     * 
+     * @param file path of file.
+     * @return extension ie (String after . ) if exists else empty String.
+     */
+    public static String getFileExtension(File file) {
+        String s = file.getName();
+        int i = s.lastIndexOf('.');
+        if (i > 0 && i < s.length() - 1) {
+            return s.substring(i + 1).toLowerCase();
+        }
+        return "";
     }
     
 }
