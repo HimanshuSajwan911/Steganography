451440a55d7615688624b2c46df95eab33824baf
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-11-17 18:49:22 +0530

    UnsupportedImageFileException Exception class.

---
+++ src/steganography/core/exceptions/UnsupportedImageFileException.java
@@ -0,0 +1,20 @@
+package steganography.core.exceptions;
+
+/**
+ * @author Himanshu Sajwan.
+ */
+
+/**
+ * UnsupportedImageFileException Exception class.
+ */
+public class UnsupportedImageFileException extends UnsupportedFileException{
+
+    public UnsupportedImageFileException() {
+        
+    }
+
+    public UnsupportedImageFileException(String msg) {
+        super(msg);
+    }
+    
+}
