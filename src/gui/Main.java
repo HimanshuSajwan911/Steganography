acb21f895a350813c850cb579f17bba687fa2af9
himanshusajwan911 <himanshusajwan911@gmail.com>
2021-12-16 18:13:32 +0530

    class to start GUI.

---
+++ src/gui/Main.java
@@ -0,0 +1,20 @@
+
+package gui;
+
+/**
+ * @author Himanshu Sajwan.
+ */
+
+public class Main {
+
+    public static MainMenu mainmenu = new MainMenu();
+    
+    public static void main(String[] args) {
+        
+        mainmenu = new MainMenu();
+        
+        mainmenu.setVisible(true);
+        
+    }
+    
+}
