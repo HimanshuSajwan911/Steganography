524a24f1c661eadb3f9cbdae38388124650acc31
himanshusajwan911 <himanshusajwan911@gmail.com>
2022-01-06 15:44:39 +0530

    InsufficientException Exception class.

---
+++ src/steganography/core/exceptions/InsufficientException.java
@@ -0,0 +1,20 @@
+package steganography.core.exceptions;
+
+/**
+ * @author Himanshu Sajwan.
+ */
+
+/**
+ * InsufficientException Exception class.
+ */
+public class InsufficientException extends Exception{
+
+    public InsufficientException() {
+        
+    }
+
+    public InsufficientException(String msg) {
+        super(msg);
+    }
+    
+}
